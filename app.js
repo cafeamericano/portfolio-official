36e8298c993d0a17a29530285b36ca84269aefdb
Developer <Developer@Matthews-MacBook-Air.local>
2019-03-19 23:38:23 -0400

    Portfolio v0.1

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var app = express()
+var handlebars = require('express-handlebars')
+
+app.use(express.static(__dirname+'/public'));
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');
+
+//ROUTES================================================================
+
+app.get('/', function(req, res) { 
+    res.render('about')
+})
+
+app.get('/apps', function(req, res) { 
+    res.render('apps')
+})
+
+app.get('/apps/studylog', function(req, res) { 
+    res.render('studylog')
+})
+
+app.get('/apps/tasklog', function(req, res) {
+    res.render('tasklog')
+})
+
+app.get('/capabilities', function(req, res) {
+    res.render('capabilities')
+})
+
+app.get('/background', function(req, res) {
+    res.render('background')
+})
+
+//START SERVER================================================================
+
+app.listen(9000, function(){
+    console.log('Server listening on Port 3000...')
+})
---
+++ public/js/animations.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#sidenav").hide();
+
+    $("#sidenavButton").click(function(){
+      $("#sidenav").toggle("slide")
+    });
+
+  });
